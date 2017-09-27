b46896f79cbbbda391b931ed249988c9a8e36e17
hsn <hsn@sendmail.cz>
2017-09-27 18:21:23 +0200

    fix compile error with latest phantom

--- src/main/scala/MacroTest.scala
+++ src/main/scala/MacroTest.scala
@@ -1,11 +1,11 @@
 package dao
 
-//import com.outworkers.phantom.database._
+import com.outworkers.phantom.macros.DatabaseHelper
 import com.outworkers.phantom.dsl._
 
 /** Base class for Phantom database with Play injected Keyspace factory */
-abstract class BasePlayDatabase[DB <: BasePlayDatabase[DB]](factory: KeyspaceDefFactory)
-      extends Database[DB](factory.create())
+abstract class BasePlayDatabase[DB <: BasePlayDatabase[DB]](factory: KeyspaceDefFactory)(implicit helper: DatabaseHelper[DB])
+      extends Database[DB](factory.create())(helper)
 
 trait KeyspaceDefFactory {
       /** create key space definition */
