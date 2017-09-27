5e89a6b5292d29c49ccd21b89b752daf17ddeeca
hsn <hsn@sendmail.cz>
2017-09-27 21:18:25 +0200

    add case for allowFiltering

--- src/main/scala/db/TokensStore.scala
+++ src/main/scala/db/TokensStore.scala
@@ -2,12 +2,25 @@ package db
 
 import com.outworkers.phantom.dsl._
 
+import java.util.UUID
+
 abstract class TokensStore extends TokensTable {
    def save(t: Token) = {
       insert
         .value(_.email, t.email)
         .value(_.token, t.token)
         .value(_.counter, t.counter)
-        .future()
+      .future()
    }
+   def get(id : UUID) = {
+      select
+         .where( _.token eqs id)
+      .one()
+   }
+   def expired(counter : Int) = {
+      select
+         .where( _.counter gte counter)
+         .allowFiltering()
+      .collect()
+   }   
 }
