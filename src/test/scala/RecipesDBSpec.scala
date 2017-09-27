27ef7b94dd41970d37b7df163c769694c723025c
hsn <hsn@sendmail.cz>
2017-09-27 18:38:45 +0200

    finish test for automatic database create

--- src/test/scala/RecipesDBSpec.scala
+++ src/test/scala/RecipesDBSpec.scala
@@ -1,10 +1,13 @@
-
+import org.scalatest.concurrent.ScalaFutures._
+import org.scalatest.Matchers._
 
 class RecipesDBSpec extends PhantomSuite {
     "Recipe database" should {
         "create cassandra tables" in {
-            
+           // if table reads return no error, we should assume that table exist
+           whenReady ( database.Recipes.findRecipeById(3) ) { case res =>
+              res should be eq (None)
+           }
         }
     }
-  
 }
