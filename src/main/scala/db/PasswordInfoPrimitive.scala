52ddef6e85eb4c513a3f28bd9365bdab46c015ea
hsn <hsn@sendmail.cz>
2017-10-09 15:32:27 +0200

    added PasswordInfo with Primitive

---
+++ src/main/scala/db/PasswordInfoPrimitive.scala
@@ -0,0 +1,14 @@
+package silhouette.db
+
+import com.outworkers.phantom.builder.primitives._
+import model.PasswordInfo
+
+object PasswordInfoPrimitive {
+   implicit val conversion : Primitive[PasswordInfo] = {
+      Primitive.derive[PasswordInfo, (String, String, Option[String])]( pi => ( pi.hasher, pi.password, pi.salt ))( x =>
+         { val ( a, b, c ) = x
+           PasswordInfo(a, b, c)
+         }
+      )
+   }
+}
