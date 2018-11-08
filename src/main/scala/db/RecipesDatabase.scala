49439c19a5c37a808170d974b489fecd2ea61273
Radim Kolar <hsn@sendmail.cz>
2018-11-08 18:53:11 +0100

    Remove table Recipe and dependant objects

--- src/main/scala/db/RecipesDatabase.scala
+++ src/main/scala/db/RecipesDatabase.scala
@@ -1,19 +1,7 @@
 package db
 
 import com.outworkers.phantom.dsl._
-import scala.concurrent.{Future => ScalaFuture}
-
-// In this section, we will show how you can create a real-world Cassandra service with com.outworkers.phantom.
-// First you have to think of what queries you need to perform. The usual.
-// Say you come up with id and author.
-
-// You will end up with several mapping tables enabling you to do all the queries you want.
-// Now you are left with maintaining consistency at application level.
-// We usually overlay a service on top of the mapping tables.
-// To keep all the complexity away from other parts of the application.
 
 class RecipesDatabase(override val connector: CassandraConnection) extends Database[RecipesDatabase](connector) {
-
-  object Recipes extends RecipesStore with Connector
-  object Tokens  extends TokensStore with Connector
+   object Tokens  extends TokensStore with Connector
 }
