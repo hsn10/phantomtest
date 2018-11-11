a73a2fdc180b2d691eddc2488317a5d53f508826
Radim Kolar <hsn@sendmail.cz>
2018-11-11 01:43:48 +0100

    unit test if database was create successfully

--- src/test/scala/db/TokensDBSpec.scala
+++ src/test/scala/db/TokensDBSpec.scala
@@ -2,12 +2,13 @@ package db
 
 import org.scalatest.concurrent.ScalaFutures._
 import org.scalatest.Matchers._
+import java.util.UUID
 
-class RecipesDBSpec extends PhantomSuite {
-    "Recipe database" should {
+class TokensDBSpec extends PhantomSuite {
+    "Tokens database" should {
         "create cassandra tables" in {
            // if table reads return no error, we should assume that table exist
-           whenReady ( database.Recipes.findRecipeById(3) ) { case res =>
+           whenReady ( database.Tokens.get(UUID.randomUUID()) ) { case res =>
               res should be eq (None)
            }
         }
