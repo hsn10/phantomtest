17d1e73e3cf3e5d7f525cb114230e0571f510fe2
Radim Kolar <hsn@sendmail.cz>
2018-11-12 14:29:42 +0100

    add delete token check

--- src/test/scala/db/TokensStoreSpec.scala
+++ src/test/scala/db/TokensStoreSpec.scala
@@ -22,5 +22,22 @@ class TokensStoreSpec extends PhantomSuite {
               ld1 should be eq (Some(t))
            }
         }
+        "delete token" in {
+           val t = Token("test2@email.com", UUID.randomUUID(), 1)
+           val chain =
+           for { save   <- database.Tokens.save(t)
+                 load   <- database.Tokens.get(t.token)
+                 delete <- database.Tokens.delete(t.token)
+                 check  <- database.Tokens.get(t.token)
+           }
+           yield ( save, load, delete, check )
+              
+           whenReady ( chain ) { case ( rs1, ld1, rs2, ld2 ) =>
+              rs1.wasApplied() shouldBe true
+              ld1 should be eq (Some(t))
+              rs2.wasApplied() shouldBe true
+              ld2 shouldBe None
+           }
+        }
     }
 }
