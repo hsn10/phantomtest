959b5da2a93e2a35885671d239adfcf8117c24dc
Radim Kolar <hsn@sendmail.cz>
2018-11-12 17:31:06 +0100

    add expire tokens test case

--- src/test/scala/db/TokensStoreSpec.scala
+++ src/test/scala/db/TokensStoreSpec.scala
@@ -1,6 +1,7 @@
 package db
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 import org.scalatest.concurrent.ScalaFutures._
 import org.scalatest.Matchers._
@@ -16,7 +17,7 @@ class TokensStoreSpec extends PhantomSuite {
                  load <- database.Tokens.get(t.token)
            }
            yield ( save, load )
-              
+
            whenReady ( chain ) { case ( rs1, ld1 ) =>
               rs1.wasApplied() shouldBe true
               ld1 should be eq (Some(t))
@@ -31,12 +32,30 @@ class TokensStoreSpec extends PhantomSuite {
                  check  <- database.Tokens.get(t.token)
            }
            yield ( save, load, delete, check )
-              
+
            whenReady ( chain ) { case ( rs1, ld1, rs2, ld2 ) =>
               rs1.wasApplied() shouldBe true
               ld1 should be eq (Some(t))
               rs2.wasApplied() shouldBe true
               ld2 shouldBe None
+           }
+        }
+        "List expired tokens correctly" in {
+           val t1 = Token("exptest1@email.com", UUID.randomUUID(), 2)
+           val t2 = Token("exptest2@email.com", UUID.randomUUID(), 3)
+           val t3 = Token("exptest3@email.com", UUID.randomUUID(), 4)
+           val t4 = Token("exptest4@email.com", UUID.randomUUID(), 5)
+
+           val f1 = database.Tokens.save(t1)
+           val f2 = database.Tokens.save(t2)
+           val f3 = database.Tokens.save(t3)
+           val f4 = database.Tokens.save(t4)
+           
+           whenReady ( Future.sequence(Seq(f1,f2,f3,f4))) { _ =>
+              whenReady ( database.Tokens.expired(4) ) { res =>
+                 res should have size (2)
+                 res should (contain(t3) and contain(t4))
+              }
            }
         }
     }
